70bf0d46f677c58fc50c451adb89dd4d8d8601e2
gardnervile <lordy1jo@gmail.com>
2024-12-07 16:41:13 +0500

    исправления

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,25 +1,19 @@
 from datacenter.models import Passcard, Visit
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from datacenter.storage_information_view import format_duration, is_visit_strange
 
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
 
-    def format_duration(duration):
-        total_seconds = int(duration.total_seconds())
-        hours = total_seconds // 3600
-        minutes = (total_seconds % 3600) // 60
-        seconds = total_seconds % 60
-        return f"{hours:02}:{minutes:02}:{seconds:02}"
-
     this_passcard_visits = []
     for visit in visits:
         entered_at = timezone.localtime(visit.entered_at)
         leaved_at = timezone.localtime(visit.leaved_at)
         duration = leaved_at - entered_at
-        is_strange = duration.total_seconds() > 3600
+        is_strange = is_visit_strange(duration)
 
         this_passcard_visits.append({
             'entered_at': entered_at.strftime('%d-%m-%Y %H:%M'),
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -3,18 +3,27 @@ from datacenter.models import Visit
 from django.shortcuts import render
 
 
-def get_duration(entered_at):
-    entered_at_local = localtime(entered_at)
-    time_in_storage = localtime() - entered_at_local
-    return time_in_storage
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+STRANGE_VISIT_THRESHOLD_SECONDS = SECONDS_IN_HOUR  # 1 час
 
 
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
+    hours = total_seconds // SECONDS_IN_HOUR
+    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
+    seconds = total_seconds % SECONDS_IN_MINUTE
     return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+def is_visit_strange(duration, threshold=STRANGE_VISIT_THRESHOLD_SECONDS):
+    return duration.total_seconds() > threshold
+
+
+def get_duration(entered_at):
+    entered_at_local = localtime(entered_at)
+    time_in_storage = localtime() - entered_at_local
+    return time_in_storage
 
 
 def storage_information_view(request):
